342bc5595fbff6824694c84aab2bf9910d7afa1f
Vyacheslav Palamar <vyacheslav.palamar@gmail.com>
2016-03-13 14:46:52 +0200

    Changed MINE constant value, refactoring (again)

--- js/model.js
+++ js/model.js
@@ -1,27 +1,31 @@
 var Field = (function () {
 
-  var MINE = 'm';
+  var MINE = -1;
   var EMPTY = 0;
 
   function Field (width, height, minesAmount) {
+
     this.width = width || 9;
     this.height = height || this.width;
     this.minesAmount = minesAmount || 14;
     this.cells = [];
+
   };
 
   Field.prototype.generate = function generateF () {
+
     for (var i = 0; i < this.height; i++) {
       this.cells[i] = [];
 
       for (var j = 0; j < this.width; j++) {
         this.cells[i][j] = EMPTY;
-
       }
     }
+
   };
 
   Field.prototype.createMines = function createMinesF () {
+
     var i = 0;
     while (i < this.minesAmount) {
 
@@ -35,9 +39,11 @@ var Field = (function () {
 
       }
     }
+
   };
 
   Field.prototype.createTips = function createTipsF () {
+
     for (var i = 1; i < this.height - 1; i++) {
 
       for (var j = 1; j < this.width - 1; j++) {
@@ -83,6 +89,7 @@ var Field = (function () {
         this.cells[i][j] = mineCounter;
       }
     }
+
   };
 
   return Field;
